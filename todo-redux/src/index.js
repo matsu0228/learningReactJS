4bdc2a7332871e62e4b5f9a46b37b6a4e41f09de
matsu0228 <m.hisa3153@gmail.com>
2019-01-29 23:40:03 +0900

    add redux sample

--- todo-redux/src/index.js
+++ todo-redux/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
@@ -10,3 +11,64 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// 1. redux sample
+const initialState = {
+  tasks: []
+};
+
+function addTasksReducer(state = initialState, action) {
+  switch (action.type) {
+    case "ADD_TASK":
+      return {
+        ...state,
+        tasks: state.tasks.concat([action.payload.task])
+      };
+    default:
+      return state;
+  }
+}
+
+function handleChange() {
+  console.log("called handleChange()", store.getState());
+  //{
+  //tasks:['Storeを学ぶ']
+  //}
+}
+
+const addTask = task => ({
+  type: "ADD_TASK",
+  payload: { task }
+});
+
+const store = createStore(addTasksReducer);
+const unsubscribe = store.subscribe(handleChange);
+
+store.dispatch(addTask("learning of Store"));
+console.log(store.getState());
+
+// 2. reset sample
+function resetReducer(state = initialState, action) {
+  switch (action.type) {
+    case "RESET_TASK":
+      return {
+        ...state,
+        tasks: []
+      };
+    default:
+      return state;
+  }
+}
+// このときはstateの変化はない
+store.replaceReducer(resetReducer);
+const resetTask = () => ({
+  type: "RESET_TASK"
+});
+// dispatchしたらstate変化
+store.dispatch(resetTask());
+
+// 3.unscbscribeしたら、handleChange()がcallされない
+console.log("unscbscribe sample");
+unsubscribe();
+store.replaceReducer(addTasksReducer);
+store.dispatch(addTask("learning of Store"));
